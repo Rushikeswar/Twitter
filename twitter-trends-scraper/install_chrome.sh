6cd6dbcfefa7b619e3b8a3079002dd984a518365
Rushikeswar <rushikesward@gmail.com>
2024-12-28 11:28:19 +0530

    final local

--- twitter-trends-scraper/install_chrome.sh
+++ twitter-trends-scraper/install_chrome.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-# Install Google Chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt-get install -y ./google-chrome-stable_current_amd64.deb
 
-# Install ChromeDriver
-CHROME_VERSION=$(google-chrome --version | awk '{print $3}' | cut -d '.' -f 1)
-wget https://chromedriver.storage.googleapis.com/$CHROME_VERSION/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip
-sudo mv chromedriver /usr/bin/
+# Update the system package list and install dependencies
+apt-get update
+apt-get install -y wget curl
+
+# Install Google Chrome
+curl -sSL https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -o google-chrome-stable_current_amd64.deb
+apt install -y ./google-chrome-stable_current_amd64.deb
+
+# Install necessary libraries for Google Chrome to work
+apt-get install -y fonts-liberation libappindicator3-1 libasound2 libnss3 libx11-xcb1 xdg-utils
